ce36ad9901ea8f474c4707162034faeebcf2c774
Kathryn Booker <kathrynbooker@me.com>
2019-06-28 16:36:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase() ;
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase() ;
 }
-function logShout(string) { // not working!!!!!
-  console.log("hello")
-  return string.toUpperCase()
+function logShout(string) { // doesn't call spy with 'HELLO'
+  console.log('hello') ;
+  spy('HELLO')
+  return string.toUpperCase() ;
 }
-function logWhisper(string) {
-  // fill in
+function logWhisper(string) { 
+  console.log('hello') ;
+  return string.toLowerCase() ;
 }
 function sayHiToGrandma(string) {
-  // fill in
+  if (string='hello') // yes
+  {
+    return "I can't hear you!" ;
+  }
+  else if (string='HELLO') // no
+  {
+    return "YES INDEED!" ;
+  }
+  else if (string='I love you, Grandma.') // no
+  {
+    return "I love you, too." ;
+  }
 }
